34447514bec42841f9695c5be1a0eeed97607193
ying <34480@ma-web.nl>
2023-05-25 14:28:45 +0200

    Added week 2 assignments

---
+++ private/opdracht-6 Oefening Switch/index.php
@@ -0,0 +1,43 @@
+<?php
+
+$month ="mei";
+
+switch ($month) {
+    case "januari":
+        echo "Today is januari.";
+        break;
+    case "februari":
+        echo "Today is februari.";
+        break;
+    case "maart":
+        echo "Today is maart.";
+        break;
+    case "april":
+        echo "Today is april.";
+        break;
+    case "mei":
+        echo "Today is mei.";
+        break;
+    case "juni":
+        echo "Today is juni.";
+        break;
+    case "juli":
+        echo "Today is juli.";
+        break;
+    case "augustus":
+        echo "Today is augustus.";
+        break;
+    case "september":
+        echo "Today is september.";
+        break;
+    case "oktober":
+        echo "Today is oktober.";
+        break;
+    case "november":
+        echo "Today is november.";
+        break;
+    case "december":
+        echo "Today is december.";
+        break;
+}
+?>
