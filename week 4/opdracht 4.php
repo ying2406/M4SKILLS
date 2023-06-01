118a8b70c06b0137cb9838268e7a6182590ec741
ying <34480@ma-web.nl>
2023-06-01 14:41:49 +0200

    Added opdracht 4

---
+++ week 4/opdracht 4.php
@@ -0,0 +1,23 @@
+<?php
+
+function showArray($array) {
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+function fillArrayWithNumbers($length = 10, $min = 0, $max = 100)
+{
+    $numbers = array();
+    
+    for ($i = 0; $i < $length; $i++) {
+        $numbers[] = rand($min, $max);
+    }
+    
+    return $numbers;
+}
+
+// Voorbeeld
+$myArray = fillArrayWithNumbers(5, 1, 10);
+print_r($myArray);
+?>
